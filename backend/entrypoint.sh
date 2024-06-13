4651e195ed5d3be255a3fbc5bf5f8a073d84a7a3
PixelOmen <PixelOmen@gmail.com>
2024-06-12 18:50:38 -0700

    fix: movedweb compose cmds into entrypoint, switch to bash

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -d /sveltedjango/media/public ]; then
     rm -rf /sveltedjango/media/public
@@ -6,5 +6,8 @@ fi
 
 mkdir -p /sveltedjango/media/public
 cp -r /sveltedjango/public/* /sveltedjango/media/public/
+
+python3 manage.py migrate
+gunicorn $PROJECT_NAME.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
